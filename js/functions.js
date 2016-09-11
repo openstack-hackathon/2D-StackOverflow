0f6239e9e56789efa569ed0fd2ac59f5e684501d
cair18 <ibagom13@hotmail.com>
2016-09-10 22:25:28 -0500

    last

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+// Dropdown Menu Fade    
+jQuery(document).ready(function(){
+    $(".dropdown").hover(
+        function() { $('.dropdown-menu', this).stop().fadeIn("fast");
+        },
+        function() { $('.dropdown-menu', this).stop().fadeOut("fast");
+    });
+});
