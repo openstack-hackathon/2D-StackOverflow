512c6cd40718849416aaf9ea830a467f675c863f
cair18 <ibagom13@hotmail.com>
2016-09-11 02:13:12 -0500

    last

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,3 @@
-// Dropdown Menu Fade    
 jQuery(document).ready(function(){
     $(".dropdown").hover(
         function() { $('.dropdown-menu', this).stop().fadeIn("fast");
@@ -6,3 +5,33 @@ jQuery(document).ready(function(){
         function() { $('.dropdown-menu', this).stop().fadeOut("fast");
     });
 });
+
+
+  function initMap() {
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          scrollwheel: false,
+          zoom: 8
+        });
+      }
+
+/*
+ function initMap() {
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: new google.maps.LatLng(20.613863, -103.416875),
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          scrollwheel: false,
+          zoom: 8
+        });
+        map.setCenter(myMarker.position);
+		myMarker.setMap(map);	
+      }
+
+  var myMarker = new google.maps.Marker({
+		position: new google.maps.LatLng(20.613863, -103.416875),
+		draggable: true
+	});
+
+*/
