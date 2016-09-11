19552a756acf78f1fb2281c47ff54e7cd7b6ee8e
Ser Digital <mariodamiancas@yahoo.com.mx>
2016-09-10 23:50:08 -0500

    Cambio a servidor de producción

--- conexion.php
+++ conexion.php
@@ -3,7 +3,8 @@
         {
             function conect()
             {
-                $serverName ="172.16.1.51";
+                //$serverName ="172.16.1.51";
+                $serverName ="172.16.11.130";
                 $connectionInfo = array( "Database"=>"rbankstackx", "UID"=>"root", "PWD"=>"root");
                 $con = mysql_connect($serverName ,$connectionInfo);
                 if (!$con)
